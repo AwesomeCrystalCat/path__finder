e1301e0962404925ea3e15a38fc75731647d97c8
Myroslava Tararuieva <mtararuiev@e2r10p16.unit.ua>
2020-01-20 23:10:19 +0200

    make updated

--- Makefile
+++ Makefile
@@ -16,6 +16,7 @@ SRCS = ./src/pathfinder.c \
 		./src/print_matrix.c \
 		./src/mx_create_matrix.c \
 		./src/mx_init_matrix.c \
+		./src/mx_dijkstra.c \
 
 SRCOUT = pathfinder.c \
 		mx_file_to_islands.c \
@@ -27,6 +28,7 @@ SRCOUT = pathfinder.c \
 		print_matrix.c \
 		mx_create_matrix.c \
 		mx_init_matrix.c \
+		mx_dijkstra.c \
 
 OBJS = ./obj/pathfinder.o \
 		./obj/mx_file_to_islands.o \
@@ -38,6 +40,7 @@ OBJS = ./obj/pathfinder.o \
 		./obj/print_matrix.o \
 		./obj/mx_create_matrix.o \
 		./obj/mx_init_matrix.o \
+		./obj/mx_dijkstra.o \
 
 OBJOUT = pathfinder.o \
 		mx_file_to_islands.o \
@@ -49,6 +52,7 @@ OBJOUT = pathfinder.o \
 		print_matrix.o \
 		mx_create_matrix.o \
 		mx_init_matrix.o \
+		mx_dijkstra.o \
 
 CFLAG = -std=c11 -Wall -Wextra -Wpedantic -Werror
 
--- src/pathfinder.c
+++ src/pathfinder.c
@@ -1,64 +1,6 @@
 #include "pathfinder.h"
 #include <stdio.h>
 
-static bool checker(int *visited) {
-    int count = 0;
-
-    for (int i = 0; visited[i] != -2147483648; i++) {
-        if (visited[i] == -1)
-            count++;
-    }
-    return count == 0 ? false : true;
-}
-
-static int *create_arr(int n, int val) {
-    int *arr = (int *)malloc(sizeof(int) * (n + 1));
-    arr[n] = -2147483648;
-    for (int i = 0; arr[i] != -2147483648; i++)
-        arr[i] = val;
-    return arr;
-}
-
-static int get_min(int *costs, int *visited, int c_min) {
-    int min = 2147483647;
-    int index = 0;
-
-    if (checker(visited) == true) {
-        for (int i = 0; costs[i] != -2147483648; i++) {
-            if (visited[i] == -1 && (costs[i] < min || costs[i] < c_min)) {
-                min = costs[i];
-                index = i;
-            }
-        }
-        return index;
-    }
-    return - 1;
-}
-
-int *mx_dijkstra(int **arr, int num, int **pred) {
-    int *costs = create_arr(num, 2147483647);
-    int *visited = create_arr(num, -1);
-    int i = 0;
-    int j;
-    int count = 0;
-
-    costs[0] = 0;
-    while (i != -1) {
-        for (j = i + 1; j < num; j++) {
-            if (arr[i][j] != -1) {
-                pred[i][j] = i;
-                if (visited[i] == -1 && visited[j] == -1
-                && arr[i][j] + costs[i] < costs[j])
-                    costs[j] = costs[i] + arr[i][j];
-            }
-        }
-        visited[i] = 1;
-        i = get_min(costs, visited, costs[i]);
-    }
-    free(visited);
-    return costs;
-}
-
 int main(int argc, char **argv) {
     char **str = mx_file_to_islands(argv[1]); //str is saved and still needed for comparison
     char **uniques = mx_find_uniques(str);    //unique created
@@ -72,8 +14,7 @@ int main(int argc, char **argv) {
     for (int i = 0; costs[i] != -2147483648; i++) {
         printf("%d\n", costs[i]);
     }
-    // print_matrix(predecessors);
-    // int *costs = mx_dijkstra(matrix, islands);
+    print_matrix(predecessors);
     system("leaks -q pathfinder");
     return 0;
 }

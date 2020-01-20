86291e1e6357cbc4cdf8e7f780b67f5313359623
Myroslava Tararuieva <mtararuiev@e2r10p16.unit.ua>
2020-01-20 04:18:19 +0200

    matrix bag

--- src/pathfinder.c
+++ src/pathfinder.c
@@ -1,28 +1,7 @@
 #include "pathfinder.h"
 #include <stdio.h>
 
-static int *create_arr(int n, int val) {
-    int *arr = (int *)malloc(sizeof(int) * (n + 1));
-    arr[n] = -2147483648;
-    for (int i = 0; arr[i] != -2147483648; i++)
-        arr[i] = val;
-    return arr;
-}
-
-static int get_min(int *costs, int *visited) {
-    int min = 2147483647;
-    int index = 0;
-
-    for (int i = 0; costs[i] != -2147483648; i++) {
-        if (visited[i] == -1 && costs[i] < min) {
-            min = costs[i];
-            index = i;
-        }
-    }
-    return index;
-}
-
-static bool stopper(int *visited) {
+static bool checker(int *visited) {
     int count = 0;
 
     for (int i = 0; visited[i] != -2147483648; i++) {
@@ -32,34 +11,62 @@ static bool stopper(int *visited) {
     return count == 0 ? false : true;
 }
 
+static int *create_arr(int n, int val) {
+    int *arr = (int *)malloc(sizeof(int) * (n + 1));
+    arr[n] = -2147483648;
+    for (int i = 0; arr[i] != -2147483648; i++)
+        arr[i] = val;
+    return arr;
+}
+
+static int get_min(int *costs, int *visited, int c_min) {
+    int min = 2147483647;
+    int index = 0;
+
+    if (checker(visited) == true) {
+        for (int i = 0; costs[i] != -2147483648; i++) {
+            if (visited[i] == -1 && (costs[i] < min || costs[i] < c_min)) {
+                min = costs[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    return - 1;
+}
+
 int *mx_dijkstra(int **arr, int num) {
     int *costs = create_arr(num, 2147483647);
     int *visited = create_arr(num, -1);
-    int cur = 1;
-    int i;
+    int i = 0;
     int j;
+    int count = 0;
 
     costs[0] = 0;
-    for (i = 0; stopper(visited) == true;) {
-        printf("iteration\n");
-        // printf("start i = %d\n", i);
+    for (; i != -1;) {
+        printf("iteration - %d\n", count++ );
         for (j = 0; j < num; j++) {
-            if (arr[i][j] != -1 && visited[j] == -1 && visited[i] == -1) {
-                if (arr[i][j] + costs[i] <  costs[cur]) {
-                    // printf("middle i = %d\n", i);
-                costs[cur] = costs[i] + arr[i][j];
-                printf("costs i = %d\n", costs[cur]);
-                cur++;
-                }
+            printf("costs j%d = %d + %d\n", costs[j], costs[i], arr[i][j]);
+            if (arr[i][j] != -1 && visited[i] == -1 && visited[j] == -1 && arr[i][j] + costs[i] < costs[j]) {
+
+
+                        printf("\n");
+                        printf("arr[i][j] = %d\n", arr[i][j]);
+                        printf("costs i = %d\n", costs[i]);
+                        
+                        
+                        costs[j] = costs[i] + arr[i][j];
+                        
+                        
+
             }
         }
             visited[i] = 1;
-        if (i < num - 1) {
-            i = get_min(costs, visited);
-            printf("end i = %d\n", i);
-        }
-        // for (int i = 0; visited[i] != -2147483648; i++)
-        //     printf("visited i = %d\n", visited[i]);
+            // for (int i = 0; visited[i] != -2147483648; i++)
+            //     printf("*%d*\n", visited[i]); 
+            i = get_min(costs, visited, costs[i]);
+            printf("i = %d\n", i);
+            // cur = i;
     }
     return costs;
 }
@@ -73,11 +80,11 @@ int main(int argc, char **argv) {
     // int **parents = mx_create_matrix(numbers);
     free(numbers);
     mx_del_strarr(&str);
-    int *costs = mx_dijkstra(matrix, islands);
-    for (int i = 0; costs[i] != -2147483648; i++) {
-        printf("%d\n", costs[i]);
-    }
-    // print_matrix(matrix);
+    // int *costs = mx_dijkstra(matrix, islands);
+    // for (int i = 0; i <= 5; i++) {
+    //     printf("%d\n", costs[i]);
+    // }
+    print_matrix(matrix);
     // int *costs = mx_dijkstra(matrix, islands);
     // system("leaks -q pathfinder");
     return 0;
